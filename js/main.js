6c4af672bd669d09fd4694e5b19fa7b3589bfa7c
chandra reddy <chandra reddy>
2018-01-02 12:55:51 +0530

    recent commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
     new WOW().init();
+    $('#work').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
 });
